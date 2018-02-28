39e84005de7c1e4fa6d2f62141a0fcf1808cf284
Yonah Elorza <ye2184@columbia.edu>
2018-02-28 16:30:15 -0500

    BCI Project beginning, ROS package setup

---
+++ BCI/build/bci_sim_arm/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/yonahel/Projects/BCI/src/bci_sim_arm
+# Build directory: /home/yonahel/Projects/BCI/build/bci_sim_arm
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ BCI/build/catkin_generated/setup_cached.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/yonahel/Projects/BCI/devel:$CMAKE_PREFIX_PATH"
+export PWD="/home/yonahel/Projects/BCI/build"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/yonahel/Projects/BCI/devel/share/common-lisp"
+export ROS_PACKAGE_PATH="/home/yonahel/Projects/BCI/src:$ROS_PACKAGE_PATH"
